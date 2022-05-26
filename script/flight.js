5670e4516bd31631510c0b72caba2376ef862192
leoinflyter <leo.ada@inflyter.com>
2022-05-26 10:58:42 +0100

    rrr

--- script/flight.js
+++ script/flight.js
@@ -1,6 +1,7 @@
 function submitFlight() {
     const flight = document.getElementById("flight").value;
     document.cookie = "flight=" + flight;
+    analytics.identify({flight:flight});
     analytics.track('Flight added', {
       flight: flight
     });
