2123ef42293e4c8e4ceacfb889fc412a36ff1fba
leoinflyter <leo.ada@inflyter.com>
2022-05-30 10:35:06 +0100

    r

--- script/sign.js
+++ script/sign.js
@@ -1,35 +1,56 @@
 function submitRegister() {
-  analytics.track('ui.sign_page.regiter_button.click param', {});
-  analytics.track('ui.sign_page.regiter_button.click sans param');
-    const login = document.getElementById("login").value;
-    const password = document.getElementById("password").value;
-    var requestOptions = {
-      method: 'POST',
-      redirect: 'follow',
-    };
-    const url = "https://server-ift-test.herokuapp.com/auth/signup?username=" + login + "&password=" + password;
-    fetch(url, requestOptions)
-      .then(response => response.text())
-      .then(result => { analytics.identify(result, { username: login }); analytics.track('Sign in', { id: result }); console.log(result); document.cookie = "id=" + result; document.cookie = "username=" + login })
-      .catch(error => console.log('error', error));
-  }
-  function submitLogin() {
-    const login = document.getElementById("login").value;
-    const password = document.getElementById("password").value;
-    var requestOptions = {
-      method: 'POST',
-      redirect: 'follow',
-    };
-    const url = "https://server-ift-test.herokuapp.com/auth/signin?username=" + login + "&password=" + password;
-    fetch(url, requestOptions)
-      .then(response => response.text())
-      .then(result => { analytics.identify(result, { username: login }); analytics.track('Login', { id: result }); console.log(result); document.cookie = "id=" + result; document.cookie = "username=" + login })
-      .catch(error => console.log('error', error));
-  }
-  function submitLogout() {
-    document.cookie = 'username=; Max-Age=0'
-    document.cookie = 'id=; Max-Age=0'
-    document.cookie = 'flight=; Max-Age=0'
-    analytics.track('Logout');
-    analytics.reset();
-  }
+  const login = document.getElementById("login").value;
+  const password = document.getElementById("password").value;
+  analytics.track("ui.sign_page.regiter_button.click param", {
+    login : login
+  });
+  var requestOptions = {
+    method: "POST",
+    redirect: "follow",
+  };
+  const url =
+    "https://server-ift-test.herokuapp.com/auth/signup?username=" +
+    login +
+    "&password=" +
+    password;
+  fetch(url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      analytics.identify(result, { username: login });
+      analytics.track("Sign in", { id: result });
+      console.log(result);
+      document.cookie = "id=" + result;
+      document.cookie = "username=" + login;
+    })
+    .catch((error) => console.log("error", error));
+}
+function submitLogin() {
+  const login = document.getElementById("login").value;
+  const password = document.getElementById("password").value;
+  var requestOptions = {
+    method: "POST",
+    redirect: "follow",
+  };
+  const url =
+    "https://server-ift-test.herokuapp.com/auth/signin?username=" +
+    login +
+    "&password=" +
+    password;
+  fetch(url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      analytics.identify(result, { username: login });
+      analytics.track("Login", { id: result });
+      console.log(result);
+      document.cookie = "id=" + result;
+      document.cookie = "username=" + login;
+    })
+    .catch((error) => console.log("error", error));
+}
+function submitLogout() {
+  document.cookie = "username=; Max-Age=0";
+  document.cookie = "id=; Max-Age=0";
+  document.cookie = "flight=; Max-Age=0";
+  analytics.track("Logout");
+  analytics.reset();
+}
